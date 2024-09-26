3fe3d0a073d334f942784c3c8476be62c2e4005c
Joe Dew <joseph_dew@icloud.com>
2024-09-26 23:18:06 +0100

    bug fixes for pushing

--- Assets/Scripts/pushing.cs
+++ Assets/Scripts/pushing.cs
@@ -7,16 +7,22 @@ using UnityEngine;
 
 public class pushing : MonoBehaviour
 {
-public GameObject player;
-public float walk_speed = 5f;
-public Transform movePointDown;
-public Transform movePointUp;
-public Transform movePointRight;
-public Transform movePointLeft;
+    public GameObject player;
+    public float walk_speed = 5f;
+    public Transform movePointDown;
+    public Transform movePointUp;
+    public Transform movePointRight;
+    public Transform movePointLeft;
 
-public float push_cooldown = 1.5f;
-private float last_used;
-   void Start()
+    public float up_push_cooldown = 1.5f;
+    public float down_push_cooldown = 1.5f;
+    public float left_push_cooldown = 1.5f;
+    public float right_push_cooldown = 1.5f;
+    private float up_last_used;
+    private float down_last_used;
+    private float right_last_used;
+    private float left_last_used;
+    void Start()
    {
     movePointDown.parent = null;
    }
@@ -40,7 +46,7 @@ private float last_used;
 
             UnityEngine.Vector3 original_pos = transform.position;
             transform.position = UnityEngine.Vector3.MoveTowards(transform.position, movePointDown.position, walk_speed * Time.deltaTime);
-            if (Input.GetAxisRaw("Vertical") == -1f && Time.time > last_used + push_cooldown)
+            if (Input.GetAxisRaw("Vertical") == -1f && Time.time > down_last_used + down_push_cooldown)
             {
 
                 if (UnityEngine.Vector3.Distance(transform.position, movePointDown.position) <= .05f)
@@ -48,7 +54,7 @@ private float last_used;
                     movePointDown.position += new UnityEngine.Vector3(0f, -1f, 0f);
                 }
 
-                last_used = Time.time;
+                down_last_used = Time.time;
             }   
         }
     }
@@ -62,7 +68,7 @@ private float last_used;
 
             UnityEngine.Vector3 original_pos = transform.position;
             transform.position = UnityEngine.Vector3.MoveTowards(transform.position, movePointUp.position, walk_speed * Time.deltaTime);
-            if (Input.GetAxisRaw("Vertical") == 1f && Time.time > last_used + push_cooldown)
+            if (Input.GetAxisRaw("Vertical") == 1f && Time.time > up_last_used + up_push_cooldown)
             {
 
                 if (UnityEngine.Vector3.Distance(transform.position, movePointUp.position) <= .05f)
@@ -70,7 +76,7 @@ private float last_used;
                     movePointUp.position += new UnityEngine.Vector3(0f, 1f, 0f);
                 }
 
-                last_used = Time.time;
+                up_last_used = Time.time;
             }
         }
     }
@@ -81,6 +87,19 @@ private float last_used;
         {
             Debug.DrawRay(gameObject.transform.position, UnityEngine.Vector3.right * 2f, Color.green, 1);
             Debug.Log("Hit!");
+
+            UnityEngine.Vector3 original_pos = transform.position;
+            transform.position = UnityEngine.Vector3.MoveTowards(transform.position, movePointLeft.position, walk_speed * Time.deltaTime);
+            if (Input.GetAxisRaw("Horizontal") == -1f && Time.time > left_last_used + left_push_cooldown)
+            {
+
+                if (UnityEngine.Vector3.Distance(transform.position, movePointLeft.position) <= .05f)
+                {
+                    movePointLeft.position += new UnityEngine.Vector3(-1f, 0f, 0f);
+                }
+
+                left_last_used = Time.time;
+            }
         }
     }
 
@@ -90,8 +109,38 @@ private float last_used;
         {
             Debug.DrawRay(gameObject.transform.position, -UnityEngine.Vector3.right * 2f, Color.green, 1);
             Debug.Log("Hit!");
+
+            UnityEngine.Vector3 original_pos = transform.position;
+            transform.position = UnityEngine.Vector3.MoveTowards(transform.position, movePointRight.position, walk_speed * Time.deltaTime);
+            if (Input.GetAxisRaw("Horizontal") == 1f && Time.time > right_last_used + right_push_cooldown)
+            {
+
+                if (UnityEngine.Vector3.Distance(transform.position, movePointRight.position) <= .05f)
+                {
+                    movePointRight.position += new UnityEngine.Vector3(1f, 0f, 0f);
+                }
+
+                right_last_used = Time.time;
+            }
         }
     }
+
+    if (uphit.collider == null && Time.time > down_last_used + down_push_cooldown)
+        {
+            movePointDown.position = transform.position;
+        }
+    if (downhit.collider == null && Time.time > up_last_used + up_push_cooldown)
+        {
+            movePointUp.position = transform.position;
+        }
+    if (righthit.collider == null && Time.time > left_last_used + left_push_cooldown)
+        {
+            movePointLeft.position = transform.position;
+        }
+    if (lefthit.collider == null && Time.time > right_last_used + right_push_cooldown)
+        {
+            movePointRight.position = transform.position;
+        }
 
    }
 
